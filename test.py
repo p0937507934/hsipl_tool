0d00794c7e71a8f7a79f28a171a4ab4cc0a819f2
p0937507934 <p0937507934@gmail.com>
2020-11-09 16:44:15 +0800

    clean some code

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import spectral.io.envi as envi
 
-file_path = r'D:\apple0820\apple0820\1-1_RT'
-im = envi.open(file_path + '.hdr', file_path + '.raw').asarray()
+
 
 
 class TestPreProcessing(TestCase):
@@ -22,11 +21,7 @@ class TestPreProcessing(TestCase):
         d = p.snv()
         assert(a.shape == d.shape)
     
-    # def test_msc(self):
-    #     a = np.linspace(1,100,224).reshape(1,224)
-    #     p = PreProcessing(a)
-    #     d = p.msc()
-    #     assert(a.shape == d.shape)
+   
 
 class TestTargetDetection(TestCase):
 
@@ -46,8 +41,7 @@ class TestTargetDetection(TestCase):
         self.assertEqual(self.img.all() , t._HIM.all())
 
     def test_cem(self):
-        # a = np.linspace(1,100,100*100*224).reshape(100,100,224)
-        # d = a[50,50,:]
+        
         self.setup()
         t =TargetDetection(self.img,self.d)
         r = t.cem()
